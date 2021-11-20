a232071ccc71be7a5f1c3a68b7d21a4613cfa59c
Gouzhong1223 <1162864960@qq.com>
2021-11-20 23:00:10 +0800

    新增 Course 服务

---
+++ dubhe-server/dubhe-course/src/main/java/org/dubhe/course/DubheCourseApplication.java
@@ -0,0 +1,25 @@
+package org.dubhe.course;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.youcode123.com
+ * @Description : Dubhe 课程服务启动类
+ * @Date : create by QingSong in 2021-11-20 14:10
+ * @Email : gouzhong1223@gmail.com
+ * @Since : JDK 1.8
+ * @PackageName : org.dubhe.course
+ * @ProjectName : Dubhe-Course
+ * @Version : 1.0.0
+ */
+@SpringBootApplication(scanBasePackages = "org.dubhe")
+@MapperScan(basePackages = {"org.dubhe.**.dao"})
+public class DubheCourseApplication {
+    public static void main(String[] args) {
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+        SpringApplication.run(DubheCourseApplication.class, args);
+    }
+}
