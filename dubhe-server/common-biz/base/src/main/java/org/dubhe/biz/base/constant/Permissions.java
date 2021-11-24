91e21990db0eb7abc1ae451af0a39a01961352fd
Gouzhong1223 <1162864960@qq.com>
2021-11-24 21:55:21 +0800

    feat(permissions): Added permission constant fields related to course management

--- dubhe-server/common-biz/base/src/main/java/org/dubhe/biz/base/constant/Permissions.java
+++ dubhe-server/common-biz/base/src/main/java/org/dubhe/biz/base/constant/Permissions.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2020 Tianshu AI Platform. All Rights Reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,6 +27,14 @@ public final class Permissions {
      * 数据管理
      */
     public static final String DATA = "hasAuthority('ROLE_data')";
+
+    /**
+     * 课程权限
+     */
+    public static final String COURSE = "hasAuthority('ROLE_course')";
+    public static final String COURSE_CREATE = "hasAuthority('ROLE_course_create')";
+    public static final String COURSE_UPDATE = "hasAuthority('ROLE_course_update')";
+    public static final String COURSE_DELETE = "hasAuthority('ROLE_course_delete')";
 
     /**
      * notebook权限
