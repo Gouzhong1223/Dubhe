d8b96148dcf6d87776e1dc1d7cd6a52393289126
Gouzhong1223 <1162864960@qq.com>
2021-12-19 21:24:45 +0800

    bugfix

---
+++ dubhe-server/dubhe-data/src/main/java/org/dubhe/data/config/cors/CorsFilter.java
@@ -0,0 +1,35 @@
+package org.dubhe.data.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description :
+ * @Date : create by QingSong in 2021-12-19 21:24
+ * @Email : gouzhong1223@gmail.com
+ * @Since : JDK 1.8
+ * @PackageName : org.dubhe.data.config.cors
+ * @ProjectName : Dubhe
+ * @Version : 1.0.0
+ */
+
+@WebFilter(filterName = "CorsFilter ")
+@Configuration
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        chain.doFilter(req, res);
+    }
+}
